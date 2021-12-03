4bd46b185311fe6b9543cf1a8f5f338794ba83c9
Clifton McCook <56374673+cliftonamccook@users.noreply.github.com>
2021-12-03 07:38:09 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,1 +1,15 @@
+<?php
 
+$host = 'localhost';
+$dbname = 'bugme';
+$username = 'root';
+$pwd = '';
+$conn;
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $pwd);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $error) {
+    echo 'Failed Database Connection<br/><br/>';
+    die($error->getMessage());
+}

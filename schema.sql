4f7bf4a9e1b4286b14266d70b5c39da34c3a701e
Clifton McCook <56374673+cliftonamccook@users.noreply.github.com>
2021-12-03 07:36:43 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,28 @@
+CREATE DATABASE bugme;
 
+USE bugme;
+
+CREATE TABLE `users` (
+    `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
+    `firstname` VARCHAR(255),
+    `lastname` VARCHAR(255),
+    `password` VARCHAR(255),
+    `email` VARCHAR(255),
+    `date_joined` DATETIME
+);
+
+CREATE TABLE `issues` (
+    `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(255),
+    `description` TEXT,
+    `type` VARCHAR(10),
+    `priority` VARCHAR(10),
+    `status` VARCHAR(20) DEFAULT 'OPEN',
+    `assigned_to` INTEGER,
+    `created_by` INTEGER,
+    `created` DATETIME,
+    `updated` DATETIME
+);
+
+INSERT INTO `users` (`firstname`, `lastname`, `password`, `email`, `date_joined`)
+VALUES ('Admin', 'User', '$2y$10$BG7vYTMyKoa2HDmcXVdJYuavGr3zLLdj0ha.3zwfadpb5PeL794m6', 'admin@project2.com', NOW());
